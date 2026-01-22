53634eecd3333b880ca8e8e0818bd7ef37ee3f37
didgori92 <didgori9@gmail.com>
2026-01-22 18:39:10 +0400

    commit

---
+++ Day 101/CW/script.js
@@ -0,0 +1,22 @@
+// cw 1
+
+let user = {
+    name: "levani",
+    lastname: "gendzekhadze",
+    age: 19
+}
+
+console.log(user)
+
+// cw 2
+let car = {
+    brand: "mercedes",
+    doilikeit: true,
+    whenitcameout: 1926
+}
+
+car.whenitcameout = 1953
+car.model = "E-class"
+delete car.doilikeit
+
+console.log(car)
