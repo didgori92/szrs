9abaa0b047343b26efb7f13be0ae7e6ed7bfd57a
didgori92 <didgori9@gmail.com>
2026-04-03 18:55:38 +0400

    commit

--- Day 110/CW/script.js
+++ Day 110/CW/script.js
@@ -1,1 +1,28 @@
-// wasnt at the lesson
+// cw1
+
+
+let list1 = [1, "hello", true, "world", 5, null, "js"]
+
+let strings = list1.filter(el => typeof el === "string")
+
+console.log(strings)
+
+
+// cw2
+
+
+let nums = [10, 50, 120, 30, 200]
+
+let res = nums.find(n => n > 100)
+
+console.log(res)
+
+
+// cw3
+
+
+let names = ["ana", "gio", "mariam", "luka", "aleqsandre"]
+
+let ind = names.findIndex(n => n.length > 5)
+
+console.log(ind)
